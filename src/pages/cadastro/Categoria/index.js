db772c6b07bd658bf27f3261b124229b1b7aa9ba
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-07-30 13:01:07 -0400

    Concluido components do form que mostrará para o usuario o nome da categoria que acabou de criar

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(['Teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+
     return (
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {nomeDaCategoria} </h1>
 
+        <form onSubmit={function handlerSubmit(infosDoEvento){
+          infosDoEvento.preventDefault();
+          setCategorias([
+            ...categorias,
+            nomeDaCategoria
+          ]);
+        }}>
+          <label>
+            Nome da Categoria:
+            <input 
+              type='text' 
+              value={nomeDaCategoria}
+              onChange={function funcaoHandler(infosDoEvento){
+                setNomeDaCategoria(infosDoEvento.target.value)
+              }}  
+            />
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+        </form>
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return(
+              <li key={`${categoria}${indice}`}>
+                {categoria}
+              </li>
+            )
+          })}
+        </ul>
         <Link to='/'>
             Voltar pra Home
         </Link>
